ef34c6b83882f027f3f5326ffb3b11f7cfad8b38
imanubdesigner <146331681+imanubdesigner@users.noreply.github.com>
2025-08-21 14:18:44 +0200

    Update hypr.sh
    Added UWSM folder

--- scripts/installer/hypr.sh
+++ scripts/installer/hypr.sh
@@ -10,8 +10,11 @@ log_message "Installation started for hypr section"
 print_info "\nStarting hypr setup..."
 print_info "\nEverything is recommended to INSTALL"
 
-run_command "pacman -S --noconfirm hyprland uwsm" "Install Hyprland (Must)" "yes"
+run_command "pacman -S --noconfirm hyprland" "Install Hyprland (Must)" "yes"
 run_command "mkdir -p /home/$SUDO_USER/.config/ && cp -r $BASE_DIR/configs/hypr /home/$SUDO_USER/.config/ && chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.config/hypr" "Copy entire hypr config folder (Must)" "yes" "no" 
+
+run_command "pacman -S --noconfirm uwsm" "Install the Universal Window Session Manager (Must)" "yes"
+run_command "mkdir -p /home/$SUDO_USER/.config/ && cp -r $BASE_DIR/configs/uwsm /home/$SUDO_USER/.config/ && chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.config/uwsm" "Copy entire uwsm config folder (Must)" "yes" "no"
 
 run_command "pacman -S --noconfirm xdg-desktop-portal-hyprland" "Install XDG desktop portal for Hyprland" "yes"
 
