95038bdfecf9f6c50b7388dfd3d55f9d282ecdf2
imanubdesigner <146331681+imanubdesigner@users.noreply.github.com>
2025-08-21 13:58:31 +0200

    Update prerequisites.sh
    Add NM, BT, WiFi, Nvidia drivers

--- scripts/installer/prerequisites.sh
+++ scripts/installer/prerequisites.sh
@@ -17,13 +17,19 @@ elif run_command "pacman -S --noconfirm --needed git base-devel" "Install YAY (M
     run_command "git clone https://aur.archlinux.org/yay.git && cd yay" "Clone YAY (Must)/Breaks the script" "no" "no" 
     run_command "makepkg --noconfirm -si && cd .. # builds with makepkg" "Build YAY (Must)/Breaks the script" "no" "no" 
 fi
-run_command "pacman -S --noconfirm pipewire wireplumber pamixer brightnessctl" "Configuring audio and brightness (Recommended)" "yes" 
+run_command "pacman -S --noconfirm linux-headers nvidia-open-dkms nvidia-utils libva libva-utils libva-nvidia-driver egl-wayland" "Install Nvidia Open DKMS" "yes"
+
+run_command "pacman -S --noconfirm pipewire pipewire-audio pipewire-alsa pipewire-jack wireplumber pamixer" "Configuring audio (Recommended)" "yes" 
 
 run_command "pacman -S --noconfirm ttf-cascadia-code-nerd ttf-cascadia-mono-nerd ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-firacode-nerd ttf-iosevka-nerd ttf-iosevkaterm-nerd ttf-jetbrains-mono-nerd ttf-jetbrains-mono ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono" "Installing Nerd Fonts and Symbols (Recommended)" "yes" 
 
 run_command "pacman -S --noconfirm sddm && systemctl enable sddm.service" "Install and enable SDDM (Recommended)" "yes"
 
-run_command "yay -S --sudoloop --noconfirm brave-bin" "Install Brave Browser" "yes" "no" 
+run_command "pacman -S --noconfirm networkmanager && systemctl enable NetworkManager.service" "Install and enable Network (Recommended)" "yes"
+
+run_command "pacman -S --noconfirm wpa_supplicant && systemctl enable wpa_supplicant.service" "Install and enable Wpa (Recommended)" "yes"
+
+run_command "pacman -S --noconfirm bluez && systemctl enable bluetooth.service" "Install and enable Bluetooth (Recommended)" "yes"
 
 run_command "pacman -S --noconfirm kitty" "Install Kitty (Recommended)" "yes"
 
