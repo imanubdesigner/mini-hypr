0778dd3932ad59f3ddc1ca5f93f57f12a8560edf
Manuel <146331681+imanubdesigner@users.noreply.github.com>
2025-08-22 16:38:54 +0200

    Update utilities.sh

--- scripts/installer/utilities.sh
+++ scripts/installer/utilities.sh
@@ -9,30 +9,25 @@ source $BASE_DIR/scripts/installer/helper.sh
 log_message "Installation started for utilities section"
 print_info "\nStarting utilities setup..."
 
-# Waybar
+# -------------------- Bar & Launcher --------------------
 run_command "pacman -S --noconfirm waybar" "Install Waybar - Status Bar" "yes"
 run_command "cp -r $BASE_DIR/configs/waybar /home/$SUDO_USER/.config/ && chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.config/waybar" "Copy Waybar config" "yes" "no"
 
-# Tofi
 run_command "yay -S --sudoloop --noconfirm tofi" "Install Tofi - Application Launcher" "yes" "no"
 run_command "cp -r $BASE_DIR/configs/tofi /home/$SUDO_USER/.config/ && chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.config/tofi" "Copy Tofi config(s)" "yes" "no"
 
-# Clipboard manager
+# -------------------- Clipboard --------------------
 run_command "pacman -S --noconfirm cliphist" "Install Cliphist - Clipboard Manager" "yes"
 
-# FastFetch
-run_command "pacman -S --noconfirm fastfetch" "Install fastfetch" "yes"
-
-# qView
-run_command "yay -S --sudoloop --noconfirm qview" "Install qView Image Viewer" "yes" "no"
-
-# SWWW
+# -------------------- Wallpapers --------------------
 run_command "yay -S --sudoloop --noconfirm swww" "Install SWWW for wallpaper management" "yes" "no"
+run_command "yay -S --sudoloop --noconfirm waypaper" "Install Waypaper - Wallpaper tool" "yes" "no"
 
-# Grimblast
+# -------------------- Screenshot --------------------
 run_command "yay -S --sudoloop --noconfirm grimblast" "Install Grimblast - Screenshot tool" "yes" "no"
 
-# Waypaper
-run_command "yay -S --sudoloop --noconfirm waypaper" "Install Waypaper - Wallpaper tool" "yes" "no"
+# -------------------- Other utilities --------------------
+run_command "yay -S --sudoloop --noconfirm qview" "Install qView - Image Viewer" "yes" "no"
+run_command "pacman -S --noconfirm fastfetch" "Install fastfetch" "yes"
 
 echo "------------------------------------------------------------------------"
