3cf4681b9b5f7e164d2356cb51dfcfaca8cfed95
Manuel <146331681+imanubdesigner@users.noreply.github.com>
2025-08-23 07:34:51 +0200

    Update theming.sh

--- scripts/installer/theming.sh
+++ scripts/installer/theming.sh
@@ -11,7 +11,7 @@ print_info "\nStarting theming setup..."
 
 run_command "pacman -S --noconfirm nwg-look" "Install nwg-look for GTK theme management" "yes" 
 
-run_command "pacman -S --noconfirm qt5ct qt6ct kvantum" "Install Qt5, Qt6 Settings, and Kvantum theme engines" "yes"
+run_command "pacman -S --noconfirm qt5ct qt6ct kvantum kvantum-qt5" "Install Qt5, Qt6 Settings, and Kvantum theme engines" "yes"
 
 run_command "mkdir -p /home/$SUDO_USER/.themes && unzip -o $BASE_DIR/assets/themes/Catppuccin-Dark-BL-MB.zip -d /home/$SUDO_USER/.themes && chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.themes" "Install Catppuccin Dark BL MB GTK theme" "yes" "no"
 
