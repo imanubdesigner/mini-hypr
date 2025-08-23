6017e86b2a0e64be8aed07f319f88b7493b5e72b
Manuel <146331681+imanubdesigner@users.noreply.github.com>
2025-08-23 06:56:38 +0200

    Update final.sh

--- scripts/installer/final.sh
+++ scripts/installer/final.sh
@@ -7,19 +7,11 @@ BASE_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")/../../")
 source $BASE_DIR/scripts/installer/helper.sh
 
 log_message "Final setup script started"
-print_bold_blue "\nCongratulations! Your Simple Hyprland setup is complete!"
+print_bold_blue "\nCongratulations! Mini Hypr setup is complete! Reboot is recommended"
 
 print_bold_blue "\nRepository Information:"
 echo "   - GitHub Repository: https://github.com/imanubdesigner/mini-hypr"
-echo "   - If you found this repo helpful, please consider giving it a star on GitHub!"
-
-print_bold_blue "\nContribute:"
-echo "   - Feel free to open issues, submit pull requests, or provide feedback."
-echo "   - Every contribution, big or small, is valuable to the community."
-
-print_bold_blue "\nTroubleshooting:"
-echo "   - If you encounter any issues, please check the GitHub issues section."
-echo "   - Don't hesitate to open a new issue if you can't find a solution to your problem."
+echo "   - This is an experiment. Seems to work well. Changes will be made to make it even better."
 
 print_success "\nEnjoy your new Hyprland environment! Reboot now for the best experience <3"
 
